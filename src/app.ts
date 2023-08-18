17965d403e6397675bd208f88db4716ce5d4a0e2
josekbm <jcblanco91@outlook.com>
2023-08-18 12:01:15 +0200

    fixing errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import  express from 'express';
 import cors from "cors";
-import bodyParser from "body-parser";
 import { bookingsRouter } from "./routes/bookingsRoutes";
 import {contactsRouter} from "./routes/contactsRoutes"
 import { roomsRouter } from "./routes/roomsRoutes";
